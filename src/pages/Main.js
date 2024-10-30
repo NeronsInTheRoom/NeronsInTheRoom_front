279a6a3860efde6ad946bf8c8c70e0b4bc825d66
LXXDJ <joker96.dj@gmail.com>
2024-10-30 18:58:44 +0900

    메인 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,9 +4,10 @@ function Main() {
       <div className="ly_logoWrap">
         <h1 className="el_logo">
           <div className="el_logo2">치매 자가진단 테스트</div>
-          <b className="hp_darkblue">M</b>emory<br /><b className="hp_darkblue">E</b>xplorer
+          너의<br/><b className="hp_darkblue">치매</b>가 보여
         </h1>
-        <a className="el_btn el_btnL el_btn__blue hp_mt100" href="/question">시작</a>
+        <a className="el_btn el_btnL el_btn__blue hp_mt70" href="/question">1분 자가진단</a>
+        <a className="el_btn el_btnL el_btn__blue hp_mt15" href="/question">정밀 자가진단</a>
       </div>
     </div>
   )
